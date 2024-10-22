6ee2d1733300e0a5188f1989e3afb4584b6d1ea0
wiggnash <vigneshwar.sigamani@gmail.com>
2024-10-23 00:06:44 +0530

    understanding the basic data types in rust

--- 3. Basic Programming Concepts/variables/src/main.rs
+++ 3. Basic Programming Concepts/variables/src/main.rs
@@ -5,4 +5,20 @@ fn main() {
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+
+    println!("Shadowing Concept");
+
+    let y = 5;
+    let y = y + 5;
+
+    {
+        let y = y * 2;
+        println!("The value of y in inner scope is: {y}");
+    }
+
+    println!("The value of y is {y}");
+
+    println!("Type infer");
+
+    let guess: u32 = "32".parse().expect("Not a number!");
 }
