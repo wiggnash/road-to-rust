b05b4ce5d91d5e2f62f97f4f5b1c79598cc12c1b
wiggnash <vigneshwar.sigamani@gmail.com>
2024-10-24 23:38:09 +0530

    finished basic data types concept

--- 3. Basic Programming Concepts/variables/src/main.rs
+++ 3. Basic Programming Concepts/variables/src/main.rs
@@ -1,3 +1,5 @@
+use std::io::{self, Read};
+
 fn main() {
     println!("Hello, world!");
 
@@ -46,10 +48,11 @@ fn main() {
     // remainder
     let remainder = 43 % 5;
 
-
-    println!("sum{sum}, difference : {difference} 
+    println!(
+        "sum{sum}, difference : {difference} 
     product : {product}, quotient : {quotient},  truncated : {truncated},
-    remainder : {remainder}");
+    remainder : {remainder}"
+    );
 
     println!("Boolean Types");
     let t = true;
@@ -57,4 +60,48 @@ fn main() {
     let f: bool = false; // with explicit type annotation
 
     println!("true : {t} false: {f}");
+
+    println!("Compound Data Types");
+    let tup = (500, 6.4, 1);
+
+    println!("Deconstructing a tuple by doing pattern matching");
+
+    let (x, y, z) = tup;
+
+    println!("The value of x is: {x}");
+    println!("The value of y is: {y}");
+    println!("The value of z is: {z}");
+
+    let five_hundred = tup.0;
+
+    let six_point_four = tup.1;
+
+    let one = tup.2;
+
+    println!("The value of five_hundred is: {five_hundred}");
+    println!("The value of six_point_four is: {six_point_four}");
+    println!("The value of one is: {one}");
+
+    println!("Compound Type 2 : Arrays");
+
+    let arr = [1, 2, 3, 4, 5];
+
+    println!("Accessing Array elements");
+
+    println!("Please enter an array index");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = arr[index];
+
+    println!("The value of the array at index {index} is : {element}");
 }
